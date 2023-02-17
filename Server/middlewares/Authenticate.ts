6c7250e65788280c91b4c1f648153beca46efc08
Harsh Keshri <harshkeshri1234567@gmail.com>
2023-02-17 18:35:03 +0530

    Added Auth MiddleWare

---
+++ Server/middlewares/Authenticate.ts
@@ -0,0 +1,31 @@
+import { Secret, verify } from "jsonwebtoken";
+
+const Authenticate = async (req: any, res: any) => {
+  try {
+    const accessToken = req.headers["authorization"].split(" ")[1];
+
+    let tempErr: any;
+    let tempUser: any;
+
+    await verify(
+      accessToken,
+      process.env.JWT_SECRET as Secret,
+      (err: any, user: any) => {
+        tempErr = err;
+        tempUser = user;
+      }
+    );
+
+    if (tempErr) return res.status(402).send({ message: "Not Authorized" });
+
+    return {
+      userName: tempUser.userName,
+      _id: tempUser._id,
+      iat: tempUser.iat,
+    };
+  } catch (e: any) {
+    return res.status(500).send(e.message);
+  }
+};
+
+export default Authenticate;
